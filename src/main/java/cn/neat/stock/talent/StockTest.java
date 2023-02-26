f2f4e6a50671d89ce912a7e4d6461f2110514414
zhangpeng <zhp_em@qq.com>
2023-02-26 14:54:12 +0800

    新增: 股票工具

---
+++ src/main/java/cn/neat/stock/talent/StockTest.java
@@ -0,0 +1,10 @@
+package cn.neat.stock.talent;
+
+/**
+ * @author 张澎_9970
+ * @date 2023/2/26 14:21 星期日
+ * @desc
+ */
+public class StockTest {
+
+}
---
+++ src/main/java/cn/neat/stock/talent/StockUtils.java
@@ -0,0 +1,11 @@
+package cn.neat.stock.talent;
+
+/**
+ * @author 张澎_9970
+ * @date 2023/2/26 14:21 星期日
+ * @desc
+ */
+public class StockUtils {
+
+
+}
